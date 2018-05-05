080e89aa8050a230631ee9db74be6c812dcbe3b1
Evan Roman <evanroman1@gmail.com>
2018-05-05 19:26:13 +0000

    restart service after deploying to server

--- deploy/update.sh
+++ deploy/update.sh
@@ -17,6 +17,11 @@ migrate_databse() {
     docker-compose -f production.yml run django 'python manage.py migrate'
 }
 
+restart_service() {
+    systemctl restart ativnos
+}
+
 update_vcs
 build_assets
 migrate_databse
+restart_service
